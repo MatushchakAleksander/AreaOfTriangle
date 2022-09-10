cc729da23a66212ce0b7172adc3abbd9d569e3c9
MatushchakAleksander <112856861+MatushchakAleksander@users.noreply.github.com>
2022-09-10 17:32:14 +0300

    Add files via upload
    My homework from lesson 4 for Hillel IT school

---
+++ AreaOfTriangle.py
@@ -0,0 +1,7 @@
+A = int(input("Enter the value of the side (base) of the triangle :  "))
+h = int(input("Enter a value for the height of the triangle :  "))
+
+result = (A*h)/2 
+print('The area of ​​the triangle is  ' + str(result))    
+
+
